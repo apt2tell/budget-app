7e137b58d92db924ea6c59350665412eed5d27e8
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-13 11:36:06 +0100

    Setup devise for app authentication

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem "pg", "~> 1.1"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
+# Use Devise for authentication for the application
+gem "devise"
+
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem "importmap-rails"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :expenditures, dependent: :destroy
   has_many :categories, dependent: :destroy
 
