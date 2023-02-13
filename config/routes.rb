cd42fa97664b659c1fcdd8e4fb3a635e0ddda595
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-13 12:38:08 +0100

    Add routes for the app resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
+  root to: 'splash#index'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "categories#index"
+  
+  resources :expenditures
+  resources :categories
 end
