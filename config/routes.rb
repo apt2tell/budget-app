169ba704d8a5750b73ca629b89f0fceac16d0f65
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-15 17:26:24 +0100

    Configure routes for devise  users

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   end
   root to: 'splashes#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  
+ 
   resources :expenditures
   resources :categories
 end
