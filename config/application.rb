a4a16bfbf3267db0a7fb8a6f488f92d68a602e5c
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-02-19 11:34:19 +0100

    Modify config/appliation.rb

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,11 @@ module BudgetApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
+    # config.before_configuration do
+    #   env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    #   YAML.load(File.open(env_file)).each do |key, value|
+    #     ENV[key.to_s] = value
+    #   end if File.exists?(env_file)
 
     # Configuration for the application, engines, and railties goes here.
     #
